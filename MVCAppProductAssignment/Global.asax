fa74d1fedcaf9a451b6b64b36103e38e79ffdca3
Soumyajit Pal <soumyajit.pal@zensar.com>
2020-08-19 20:38:07 +0530

    Add MVCAppProductAssignment folder

---
+++ MVCAppProductAssignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAppProductAssignment.MvcApplication" Language="C#" %>
---
+++ MVCAppProductAssignment/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAppProductAssignment.Models
+{
+    public class Product
+    {
+        public int Product_Id { get; set; }
+        public String Product_Name { get; set; }
+
+        public double Rate { get; set; }
+    }
+}
